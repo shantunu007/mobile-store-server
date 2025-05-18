7cd4b8e4da0d8efc22547e50ea9afcce96464828
shantunu007 <chakmashantunu.web@gmail.com>
2025-05-18 23:43:36 +0600

    remove await.client

--- index.js
+++ index.js
@@ -38,9 +38,9 @@ async function run() {
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
+    } catch (error) {
         // Ensures that the client will close when you finish/error
-        await client.close();
+         console.error("MongoDB connection error:", error);
     }
 }
 run().catch(console.dir);
